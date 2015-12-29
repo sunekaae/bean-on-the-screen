46c9789ec485cd1cb68fc0872be5d6dda14c750a
Sune Kaae <ska@mac-ska-mbp121.bredbandsbolaget.se>
2015-12-29 19:08:27 +0100

    now changing image every 5 secs

--- spike1/ViewController.m
+++ spike1/ViewController.m
@@ -16,31 +16,28 @@
 
 
 
-
-
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-
-    NSInteger randomInt = arc4random_uniform(3);
-
-    NSString* imageUrl = [self getImageUrl:randomInt];
-    imageView.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:imageUrl]]];
-
-    
     // http://stackoverflow.com/questions/7700352/repeating-a-method-every-few-seconds-in-objective-c
-    [NSTimer scheduledTimerWithTimeInterval:5.0 target:self selector:@selector(foo) userInfo:nil repeats:YES];
-    
-    
+    [NSTimer scheduledTimerWithTimeInterval:5.0 target:self selector:@selector(tick) userInfo:nil repeats:YES];
 }
 
 -(NSString*)getImageUrl:(NSInteger)imageNumber {
-    NSArray *imageUrls = @[@"https://www.adaptiveshop.se/uploads/news/53/ssl.png", @"https://7star.se/wp-content/uploads/2014/08/SSL-Security.jpg", @"https://upload.wikimedia.org/wikipedia/commons/0/03/SSL_SL6000E_and_baby_-_20090722.jpg"];
+    NSArray *imageUrls = @[
+                           @"https://www.adaptiveshop.se/uploads/news/53/ssl.png",
+                           @"https://7star.se/wp-content/uploads/2014/08/SSL-Security.jpg",
+                           @"https://upload.wikimedia.org/wikipedia/commons/0/03/SSL_SL6000E_and_baby_-_20090722.jpg"];
     return imageUrls[imageNumber];
 }
 
--(void)foo {
-    NSLog(@"testsune");
+-(void)tick {
+    NSLog(@"tick...");
+    // http://stackoverflow.com/questions/160890/generating-random-numbers-in-objective-c
+    NSInteger randomInt = arc4random_uniform(3);
+    
+    NSString* imageUrl = [self getImageUrl:randomInt];
+    imageView.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:imageUrl]]];
+
 }
 
 - (void)didReceiveMemoryWarning {

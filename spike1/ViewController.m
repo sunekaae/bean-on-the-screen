14a6c155c02be8ad2c62873cebbca81e59319565
Sune Kaae <ska@mac-ska-mbp121.bredbandsbolaget.se>
2015-12-29 21:10:43 +0100

    adding exception for http (not ssl)

--- spike1/ViewController.m
+++ spike1/ViewController.m
@@ -22,22 +22,27 @@
     [NSTimer scheduledTimerWithTimeInterval:5.0 target:self selector:@selector(tick) userInfo:nil repeats:YES];
 }
 
--(NSString*)getImageUrl:(NSInteger)imageNumber {
+-(NSString*)getRandomImageUrl{
+    // http://stackoverflow.com/questions/160890/generating-random-numbers-in-objective-c
     NSArray *imageUrls = @[
                            @"https://www.adaptiveshop.se/uploads/news/53/ssl.png",
                            @"https://7star.se/wp-content/uploads/2014/08/SSL-Security.jpg",
                            @"https://upload.wikimedia.org/wikipedia/commons/0/03/SSL_SL6000E_and_baby_-_20090722.jpg"];
-    return imageUrls[imageNumber];
+    imageUrls = @[@"http://weknowyourdreams.com/images/babies/babies-04.jpg"];
+    NSInteger randomInt = arc4random_uniform(imageUrls.count);
+    return imageUrls[randomInt];
 }
 
 -(void)tick {
     NSLog(@"tick...");
-    // http://stackoverflow.com/questions/160890/generating-random-numbers-in-objective-c
-    NSInteger randomInt = arc4random_uniform(3);
     
-    NSString* imageUrl = [self getImageUrl:randomInt];
-    imageView.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:imageUrl]]];
+    NSString* imageUrl = [self getRandomImageUrl];
+    [self setImageOnScreen:imageUrl];
+}
 
+-(void)setImageOnScreen:(NSString*)imageUrl {
+    NSLog(@"About to load image: %@ ", imageUrl);
+    imageView.image = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:imageUrl]]];
 }
 
 - (void)didReceiveMemoryWarning {
